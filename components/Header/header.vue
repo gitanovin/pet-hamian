caac0165a0c15f8608738e4672936f5fa0837363
Mahboub Hosseinzadeh <mahboubhosseinzadeh@gmail.com>
2024-04-06 17:16:28 +0330

    update

---
+++ components/Header/header.vue
@@ -0,0 +1,7 @@
+<template>
+<header>
+<div class="flex items-center gap-4 justify-between">
+
+</div>
+</header>
+</template>
---
+++ components/Home/home.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="slider">
+
+    </section>
+
+    <section class="Kind-appointments">
+
+    </section>
+
+    <section class="Introduction-sponsors">
+
+    </section>
+
+    <section class="Sign-Up">
+
+    </section>
+
+</template>
