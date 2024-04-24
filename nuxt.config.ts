cd06bd55350779ae6c664f6c1ba7fb11c74c32ce
Mahboub Hosseinzadeh <91733981+mahboubhosseinzadeh@users.noreply.github.com>
2024-04-24 15:15:20 +0330

    بروز شد

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,34 +1,33 @@
 
 export default defineNuxtConfig({
-    css: ['~/assets/css/app.css','~/assets/css/fonts/icons/icons.css'],
-    postcss: {
-      plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-        
-      },
-    },
-  
+  css: ['~/assets/css/app.css', '~/assets/css/fonts/icons/icons.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
 
-  
-    app: {
-      head: {
-        charset: 'utf-8',
-        viewport: 'width=device-width, initial-scale=1',
-        htmlAttrs: {
-          dir: 'rtl'
-        },
-        title: 'Pet Strore App',
-        meta: [
-          { name: 'description', content: 'Pet Store Website' }
-        ],
-        bodyAttrs: {
-          class: 'font-fa'
-        },
-      }
-      
-      
     },
-  
-  })
-  
+  },
+
+
+
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      htmlAttrs: {
+        dir: 'rtl'
+      },
+      title: 'Pet Strore App',
+      meta: [
+        { name: 'description', content: 'Pet Store Website' }
+      ],
+      bodyAttrs: {
+        class: 'font-fa'
+      },
+    }
+
+
+  },
+
+})
